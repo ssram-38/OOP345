1a78fc3ee1135084d10505bbf1745735eafada7b
ssram-38 <ssram@myseneca.ca>
2024-05-14 16:54:34 -0400

    (output fix) added missing space from the first output

--- Workshops/Workshop_1/Workshop_1/w1_p1.cpp
+++ Workshops/Workshop_1/Workshop_1/w1_p1.cpp
@@ -34,7 +34,7 @@ int main(int argc, char* argv[])
 	//   3: third argument
 	//   ...
 	for (int i = 0; i < argc; i++) {
-		std::cout << i + 1 << ": " << argv[i] << std::endl;
+		std::cout << "  " << i + 1 << ": " << argv[i] << std::endl;
 	}
 
 	std::cout << "--------------------------\n\n";
